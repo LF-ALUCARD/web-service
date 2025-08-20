9077c0993e068e77701718c15f5b178af1a833cc
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-20 16:48:57 -0300

    criação do OrderStatus

--- src/main/java/com/luizfelipe/one_project/entities/Order.java
+++ src/main/java/com/luizfelipe/one_project/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.luizfelipe.one_project.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,13 +29,16 @@ public class Order implements Serializable{
 	@JoinColumn (name = "clientes_id")
 	private User client;
 	
+	private int orderStatus;
+	
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus , User client) {
 		super();
 		Id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -59,6 +64,14 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
---
+++ src/main/java/com/luizfelipe/one_project/entities/enums/OrderStatus.java
@@ -0,0 +1,31 @@
+package com.luizfelipe.one_project.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int code;
+	
+	private OrderStatus (int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		
+		for (OrderStatus x : OrderStatus.values()) {
+			
+			if (x.getCode() == code) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Códico Invalido");
+	}
+}
