918dbddd46c3e1b86457e6fb9759d443eb6e8430
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-27 19:11:53 -0300

    criando entidade Payment

--- src/main/java/com/luizfelipe/one_project/configuration/TestConfg.java
+++ src/main/java/com/luizfelipe/one_project/configuration/TestConfg.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luizfelipe.one_project.entities.Category;
 import com.luizfelipe.one_project.entities.Order;
 import com.luizfelipe.one_project.entities.OrderItem;
+import com.luizfelipe.one_project.entities.Payment;
 import com.luizfelipe.one_project.entities.Product;
 import com.luizfelipe.one_project.entities.User;
 import com.luizfelipe.one_project.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfg implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T16:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		this.orderRepository.saveAll(Arrays.asList(o1));
 	}
 	
 	
