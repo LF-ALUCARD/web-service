231f7da463c60f8ef24d4252952a340dd8db7233
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-31 11:42:54 -0300

    atualizado usuarios

--- src/main/java/com/luizfelipe/one_project/resources/UserResources.java
+++ src/main/java/com/luizfelipe/one_project/resources/UserResources.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class UserResources {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{Id}")
 	public ResponseEntity<User> findById(@PathVariable Long Id){
 		
 		User obj = userService.findById(Id);
@@ -51,5 +52,11 @@ public class UserResources {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping (value = "/{id}")
+	public ResponseEntity<User> update (@PathVariable Long id, @RequestBody User obj){
+		obj = userService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
